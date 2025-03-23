3cd5939863778aee3f70546fcde40c5d4ad70ce9
v.nam <v4lentine.nam@gmail.com>
2025-03-23 18:58:26 +0300

    add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,69 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+'use strict'
 
-export default {
-  mode: process.env.NODE_ENV || 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
-    ],
+const path = require('path');
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+// const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  entry: path.resolve(__dirname,  'src', 'index.js'),
+  stats: 'warning:false',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: 'index.[contenthash].js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8000,
+    hot: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: path.resolve(__dirname,'index.html' )
     }),
+    // new miniCssExtractPlugin()
   ],
-  output: {
-    clean: true,
-  },
-};
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'postcss-loader',
+        // test: /\.(scss)$/,
+  //       use: [
+  //         {
+  //           // Extracts CSS for each JS file that includes CSS
+  //           loader: miniCssExtractPlugin.loader
+  //         },
+  //         {
+  //           // Interprets `@import` and `url()` like `import/require()` and will resolve them
+  //           loader: 'css-loader'
+  //         },
+  //         {
+  //           // Loader for webpack to process CSS with PostCSS
+  //           loader: 'postcss-loader',
+  //           options: {
+  //             postcssOptions: {
+  //               plugins: [
+  //                 autoprefixer
+  //               ]
+  //             }
+  //           }
+  //         },
+  //         {
+  //           // Loads a SASS/SCSS file and compiles it to CSS
+  //           loader: 'sass-loader'
+  //         }
+  //       ]
+      }
+    ]
+  }
+}
